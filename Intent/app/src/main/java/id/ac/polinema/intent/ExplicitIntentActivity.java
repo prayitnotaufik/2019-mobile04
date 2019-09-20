87c0fda341d831e870204d9f3abf1102813c0382
Muhammad Taufik Prayitno <taufikprayitno16@gmail.com>
2019-09-20 15:18:54 +0700

    Add Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,20 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText nameInput;
+    private TextView nameOutput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nameInput = findViewById(R.id.input_name);
+        nameOutput = findViewById(R.id.text_output);
+
     }
 
+    public void handleSubmit(View view) {
+        String nama = nameInput.getText().toString();
+        nameOutput.setText("Congratulations " + nama);
+    }
 }
